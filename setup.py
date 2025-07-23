cdb0b6856055c2ca772fcec406e496c890b09aef
Deborah Johnson <deborahjohnsonere@gmail.com>
2025-07-23 22:52:08 +0100

    added requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="airline_chatbot",
+    version="0.1.0",
+    author="Deborah Johnson",
+    author_email="debbiejohnsonere@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
